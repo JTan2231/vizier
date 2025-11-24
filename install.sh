947eb4a77198ce9ac7ff8619ed41c095d170649e
Joey Tan <j.tan2231@gmail.com>
2025-11-24 16:59:58 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cargo build --release
 
 sudo cp target/release/vizier /usr/local/bin/
 sudo mkdir -p /usr/local/share/vizier/agents
-sudo cp examples/agents/*.sh /usr/local/share/vizier/agents/
-sudo chmod +x /usr/local/share/vizier/agents/*.sh
+sudo cp -R examples/agents/* /usr/local/share/vizier/agents/
+sudo find /usr/local/share/vizier/agents -type f -name "*.sh" -exec chmod +x {} +
